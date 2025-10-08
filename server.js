b02eb74001cc29c4a327aebcd1d177688fa7b168
Isaac Akinsanya <IsaacAki247@gmail.com>
2025-10-08 15:10:56 +0100

    feat: created server.js
    Created an express app and set up port using environmental variables
    
    Set the assets path to the views folder. Enabled ejs as the view engine.
    
    Using the public folder to access js and css files
    
    Set urlencodedto true to parse form data
    
    Set up routes

---
+++ server.js
@@ -0,0 +1,33 @@
+import 'dotenv/config.js';
+import path from 'node:path';
+import express from 'express';
+import { fileURLToPath } from 'node:url';
+import indexRouter from './src/routes/index-routes.js';
+import treatmentsRouter from './src/routes/treatment-routes.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const assetsPath = path.join(__dirname, 'src/public'); // for accessing css, js and assets
+
+app.set('views', [
+    path.join(__dirname, 'src/views'),
+    path.join(__dirname, 'src/views/treatments')
+]);
+app.set('view engine', 'ejs'); // views template using ejs
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true })); // needed to parse form data into req.body
+
+app.use('/', indexRouter);
+app.use('/treatments', treatmentsRouter);
+
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.error(error);
+    }
+
+    console.log(`Server running on port: http://localhost:${PORT}`);
+});
