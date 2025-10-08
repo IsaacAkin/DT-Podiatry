2795fbbd252883766d3eb3ad65616feef94d290c
Isaac Akinsanya <IsaacAki247@gmail.com>
2025-10-08 15:43:43 +0100

    feat(root pages): set up routes for all top-level pages
    created an express Router
    
    set up page routes for home, about and contact-us
    
    stored page links for homepage and treatment pages as objects in their respective arrays to be retrieved in their ejs files

---
+++ src/routes/index-routes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { handleContactForm } from "../controllers/index-controller.js";
+
+const router = express.Router();
+
+const navLinks = [
+    { href: '/contact-us', text: 'Contact Us' },
+    { href: '/about', text: 'About' },
+]
+
+const treatmentLinks = [
+    { href: '/treatments/nail-surgery', text: 'Nail Surgery' },
+    { href: '/treatments/biomechanics', text: 'Biomechanics' },
+    { href: '/treatments/orthotics', text: 'Orthotics' },
+]
+
+router.get('/', (req, res) => res.render('index', { navLinks, treatmentLinks }));
+router.get('/about', (req, res) => res.render('about', { navLinks, treatmentLinks }));
+router.get('/contact-us', (req, res) => res.render('contact-us', { navLinks, treatmentLinks }));
+router.post('/contact-us', handleContactForm);
+
+export default router;
