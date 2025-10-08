1a292f268d61506b7ecb31f270eb0c6e5fbf29d8
Isaac Akinsanya <IsaacAki247@gmail.com>
2025-10-08 18:20:10 +0100

    feat(treatment routes):
    created an express Router
    
    set up page routes for all treatment pages
    
    stored page links for homepage and treatment pages as objects in their respective arrays to be retrieved in their ejs files

---
+++ src/routes/treatment-routes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { handleNailSurgeryForm, handleBiomechanicsForm, handleOrthoticsForm, sendEmail } from "../controllers/treatment-controllers.js";
+
+const router = express.Router();
+
+const navLinks = [
+    { href: '/contact-us', text: 'Contact Us' },
+    { href: '/about', text: 'About' },
+]
+
+const treatmentLinks = [
+    { href: '/treatments/nail-surgery', text: 'Nail Surgery' },
+    { href: '/treatments/biomechanics', text: 'Biomechanics' },
+    { href: '/treatments/orthotics', text: 'Orthotics' },
+]
+
+router.get('/', (req, res) => res.render('treatments', { navLinks, treatmentLinks }));
+router.get('/nail-surgery', (req, res) => res.render('nail-surgery', { navLinks, treatmentLinks, formRoute: '/treatments/nail-surgery' }));
+router.post('/nail-surgery', handleNailSurgeryForm);
+router.get('/biomechanics', (req, res) => res.render('biomechanics', { navLinks, treatmentLinks, formRoute: '/treatments/biomechanics' }));
+router.post('/biomechanics', handleBiomechanicsForm);
+router.get('/orthotics', (req, res) => res.render('orthotics', { navLinks, treatmentLinks, formRoute: '/treatments/orthotics' }));
+// router.post('/orthotics', handleOrthoticsForm);
+router.post('/orthotics', sendEmail);
+
+export default router;
